d524e359f934dbc83a6adc9af3a4883fd90c0814
gotsui <krhamui.2016@gmail.com>
2024-10-11 00:05:30 +0900

    タスクエリアをコンポーネント化

--- web-react-rust/react/react_app/src/App.tsx
+++ web-react-rust/react/react_app/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import './style.css';
 import { start } from "repl";
+import { GanttTask } from "./conponents/GanttTask";
 import { GanttDate } from "./conponents/GanttDate";
 import { GanttBarArea } from "./conponents/GanttBarArea";
 
@@ -524,47 +525,7 @@ function App() {
 					月
 				</div>
 				<div id="gantt-content" className="flex">
-					<div id="gantt-task">
-						<div id="gantt-task-title" className="flex items-center bg-green-600 text-white h-20">
-							<div className="border-t border-r border-b flex items-center justify-center font-bold text-xs w-48 h-full">タスク</div>
-							<div className="border-t border-r border-b flex items-center justify-center font-bold text-xs w-24 h-full">開始日</div>
-							<div className="border-t border-r border-b flex items-center justify-center font-bold text-xs w-24 h-full">終了日</div>
-							<div className="border-t border-r border-b flex items-center justify-center font-bold text-xs w-24 h-full">担当者</div>
-							<div className="border-t border-r border-b flex items-center justify-center font-bold text-xs w-24 h-full">進捗</div>
-						</div>
-						<div id="gantt-task-list" className="overflow-y-hidden" style={{height: `${calendarSize.height}px`}}>
-							{displayTasks.map((task: any, index: any) => {
-								return (
-									<div key={index} className="flex h-10 border-b">
-										{task.cat === 'category' &&
-											<div className="flex items-center font-bold w-full text-sm pl-2">
-												{task.name}
-											</div>
-										}
-										{task.cat === 'task' &&
-											<>
-												<div className="border-r flex items-center font-bold w-48 text-sm pl-4">
-													{task.name}
-												</div>
-												<div className="border-r flex items-center justify-center w-24 text-sm">
-													{task.start_date}
-												</div>
-												<div className="border-r flex items-center justify-center w-24 text-sm">
-													{task.end_date}
-												</div>
-												<div className="border-r flex items-center justify-center w-24 text-sm">
-													{task.incharge_user}
-												</div>
-												<div className="border-r flex items-center justify-center w-24 text-sm">
-													{task.percentage}%
-												</div>
-											</>
-										}
-									</div>
-								)
-							})}
-						</div>
-					</div>
+					<GanttTask displayTasks={displayTasks} calendarSize={calendarSize} />
 					<div id="gantt-calendar" className="overflow-x-scroll overflow-y-hidden border-l" style={{width: `${calendarSize.width}px`}}>
 						<GanttDate calendars={calendars} calendarSize={calendarSize} blockSize={blockSize} />
 						<GanttBarArea calendarSize={calendarSize} taskBars={taskBars} onMouseDownMove={handleMouseDownMove} onMouseDownResize={handleMouseDownResize} />
